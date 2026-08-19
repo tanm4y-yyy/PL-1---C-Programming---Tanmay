6ace25974248ce9324679cc99708c7f6ffeb830e
tanm4y-yyy <tshinde7654@gmail.com>
2026-08-20 02:01:38 +0530

    Add files via upload

---
+++ TVS5.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float average;
+float a=33;
+float b=47;
+float c=20;
+clrscr();
+average =(a+b+c)/3;
+printf("\n Average=%f",average);
+getch();
+}
